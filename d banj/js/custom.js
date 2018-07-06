1cc67659a9e9bf2410d3657329e8a5c4091f9c97
Officialbella <ikechukwusam007@gmail.com>
2018-07-06 08:03:38 -0700

    new push to repo

---
+++ d banj/js/custom.js
@@ -0,0 +1,70 @@
+
+$(function () {
+    $('.counter').counterUp({
+        delay: 100,
+        time: 2000,
+    });
+
+});
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+  var dataText = [ "I am a fully professional freelance creative User ", "Interface Designer & Developer Involvingwith latest web", "designing and technologies is great feel free to contact creative .", "Am also for hire on Personal Site and Project" ,"Check out my Social Media handle Below", " Thanks for Visting ,I hope to do some business with you... "];
+  
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
+
+
+
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get/return id like #about, #work, #team and etc
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top -55
+        }, 1250);
+    });
+});
+
---
+++ d banj/js/map.js
@@ -0,0 +1,8 @@
+function initMap() {
+	var location = {lat: 6.524379, lng: 3.379206};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center:location
+	});
+}
+
